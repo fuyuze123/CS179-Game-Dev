0e6552ec47b20fc1ae8282271c1a28615b92b4f6
miketinnnn <michaeltin583@gmail.com>
2025-04-22 02:06:35 -0700

    Update PlayerHealth.cs to be consistent with other code

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,21 +1,39 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public static PlayerHealth instance;
+
+    [Header("Events")]
+    public static UnityEvent<int> onPlayerHealthChange = new UnityEvent<int>();
+    
     public int maxHealth = 100;
     public int currentHealth;
 
     private void Awake()
     {
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
         currentHealth = maxHealth;
+        onPlayerHealthChange.Invoke(currentHealth);
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         Debug.Log("Damage Taken: " + damage); // Debug
+        onPlayerHealthChange.Invoke(currentHealth);
         if (currentHealth <= 0)
         {
             Die();
