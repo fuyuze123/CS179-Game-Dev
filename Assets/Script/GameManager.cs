2ecf59bcf59b75995bfdc7e41bf30dcc52e4c4a2
Augustine Ayoub <ayoubraugustine@gmail.com>
2025-05-08 17:57:59 -0700

    added boss enemy to every 5 waves

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GAME OVER");
+        Time.timeScale = 0f; 
+    }
+}
